9b36517fd0a704a8e59dbcce04f38ef37aa2cee2
chaos全局系统用户名 <hashmap2018@163.com>
2019-07-12 22:57:35 +0800

    首次提交git-project

---
+++ git-project/src/com/ch/git/GitTest.java
@@ -0,0 +1,19 @@
+package com.ch.git;
+
+public class GitTest {
+
+    private static String str= null;
+
+    public static void main(String[] args) {
+        System.out.println("Hello Git Project");
+        System.out.println("Hello Git Project");
+        System.out.println("Hello Git Project");
+        System.out.println("Hello Git Project");
+        System.out.println("Hello Git Project");
+        System.out.println("Hello Git Project");
+        System.out.println("Hello Git Project");
+    }
+/*    static {
+        str = new String("hello");
+    }*/
+}
